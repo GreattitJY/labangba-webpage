50f678e19ca2212db465e4d04e75f137e030024d
GreattitJY <great.tit.jaejae@gmail.com>
2023-01-27 11:16:12 +0900

    feat: header 부분 링크 롤링 기능 추가(더미 데이터 연결 및 즉시 실행함수 적용)

---
+++ js/headerText.js
@@ -0,0 +1,30 @@
+const renderText = async () => {
+  try {
+    let dataArray = [];
+    const res = await fetch("../data/rollingText.json");
+    const result = await res.json();
+
+    for (const data of result) {
+      dataArray.push({ ...data });
+    }
+    return dataArray;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const rollingText = (async () => {
+  const $headerText = document.querySelector(".link-goods");
+  const dataArray = await renderText();
+  let index = 0;
+
+  const rolling = setInterval(() => {
+    if (index < dataArray.length - 1) {
+      index++;
+    } else {
+      index = 0;
+    }
+    $headerText.textContent = dataArray[index].title;
+    $headerText.setAttribute("href", dataArray[index].link);
+  }, 5000);
+})();
