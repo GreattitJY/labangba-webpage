68ebae0838556dc142a4157e02b9701b3206ddda
GreattitJY <great.tit.jaejae@gmail.com>
2023-01-30 23:38:27 +0900

    feat: 1000px 이상 1200px 미만 sidebar 마우스오버 이벤트 추가

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -65,20 +65,39 @@ $btnOpne.addEventListener("click", () => {
   sidebarShow();
 });
 
+const mouseIn = () => {
+  sidebarOpen();
+};
+const mouseOut = () => {
+  $sidebarWrap.style.display = "block";
+  $body.style.paddingLeft = "64px";
+  sidebarMini();
+  sidebarView();
+};
+
 window.onresize = function () {
   const width = window.innerWidth;
 
   if (width >= 1200) {
     $body.style.paddingLeft = "180px";
     sidebarOpen();
+
+    $sidebarWrap.removeEventListener("mouseover", mouseIn);
+    $sidebarWrap.removeEventListener("mouseout", mouseOut);
   } else if (width >= 1000 && width < 1200) {
     $sidebarWrap.style.display = "block";
     $body.style.paddingLeft = "64px";
     sidebarMini();
     sidebarView();
+
+    $sidebarWrap.addEventListener("mouseover", mouseIn);
+    $sidebarWrap.addEventListener("mouseout", mouseOut);
   } else {
     $sidebarWrap.style.display = "none";
     $body.style.paddingLeft = "0px";
+
+    $sidebarWrap.removeEventListener("mouseover", mouseIn);
+    $sidebarWrap.removeEventListener("mouseout", mouseOut);
   }
 };
 
