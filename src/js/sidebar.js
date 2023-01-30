08003118d51fdfe64ff7d057ed803fbc8c0f9a41
GreattitJY <great.tit.jaejae@gmail.com>
2023-01-30 21:21:24 +0900

    feat: sidebar onresize 이벤트 추가 (닫기 버튼 시 레이아웃 수정 필요)

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,15 +1,36 @@
 const sidebarClose = (() => {
   const $btnClose = document.querySelector(".sidebar-close");
   const $sidebarWrap = document.querySelector(".sidebar-wrap");
+  const sidebarBlack = matchMedia("screen and (min-width: 1000px)");
+  const $sidebarLinkText = document.querySelectorAll(".sidebar-section a > span");
+
   $btnClose.addEventListener("click", () => {
-    $sidebarWrap.style.display = "none";
+    if (sidebarBlack.matches) {
+      [...$sidebarLinkText].map((span) => (span.style.display = "none"));
+    } else {
+      $sidebarWrap.style.display = "none";
+    }
   });
 })();
 
 const sidebarOpen = (() => {
   const $btnOpne = document.querySelector(".btn-sidebar");
   const $sidebarWrap = document.querySelector(".sidebar-wrap");
+  const $sidebarLinkText = document.querySelectorAll(".sidebar-section a > span");
+
   $btnOpne.addEventListener("click", () => {
     $sidebarWrap.style.display = "block";
+    [...$sidebarLinkText].map((span) => (span.style.display = "inline"));
   });
 })();
+
+window.onresize = function () {
+  const width = window.innerWidth;
+  const $sidebarWrap = document.querySelector(".sidebar-wrap");
+
+  if (width >= 1000) {
+    $sidebarWrap.style.display = "block";
+  } else {
+    $sidebarWrap.style.display = "none";
+  }
+};
